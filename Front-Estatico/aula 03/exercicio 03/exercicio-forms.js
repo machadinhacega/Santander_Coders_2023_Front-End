c12c0947c8f631ae8caa5e56786fe50b38eaefda
Iza Machado <izav.machado@gmail.com>
2023-07-20 10:36:36 -0300

    aula 04

--- Front-Estatico/aula 03/exercicio 03/exercicio-forms.js
+++ Front-Estatico/aula 03/exercicio 03/exercicio-forms.js
@@ -1,64 +1,3 @@
-// seleciona o formulario com getElementById
-// a função .addEventListener recebe 2 parâmetros:
-// 1º: um alerta, que no caso é o "submit" (qndo o botao for disparado)
-// 2º: uma função
-
-
-// pra não atualziar a página por conta do "submit" no button
-
-// pega o email pela id
-
-
-
-// transforma em JSON
-
-
-// vai dar undefined pq nao é mais um objeto e sim uma sting
-
-// pra reutilizar "como objeto"
-
-
-
-// document.getElementById("meuFormulario").addEventListener("submit", function(event){
-//     event.preventDefault();
-//     var name = document.getElementById("nome").value
-//     console.log(name)
-// })
-
-
-
-{/* <script>
-// seleciona o formulario com getElementById
-// a função .addEventListener recebe 2 parâmetros:
-    // 1º: um alerta, que no caso é o "submit" (qndo o botao for disparado)
-    // 2º: uma função
-document.getElementById("meuFormulario").addEventListener("submit", function(event){
-    // pra não atualziar a página por conta do "submit" no button
-    event.preventDefault();
-    // pega o email pela id
-    var email = document.getElementById("email").value
-    var password = document.getElementById("password").value
-    
-    var dados = {
-        email: email,
-        password: password,
-    };
-    // transforma em JSON
-    const data = JSON.stringify(dados)
-
-    localStorage.setItem("dadosFormulario", data)
-    
-    // vai dar undefined pq nao é mais um objeto e sim uma sting
-    console.log(data.email)
-    // pra reutilizar "como objeto"
-    const dataResgatada = JSON.parse(data)
-    console.log('Parse: ',dataResgatada.email)
-})
-</script> */}
-
-
-
-
 document.getElementById("meuFormulario").addEventListener("submit", function(event){
     event.preventDefault();
     
@@ -80,17 +19,4 @@ document.getElementById("meuFormulario").addEventListener("submit", function(eve
 
 
 
-// document.getElementById("meuFormulario").addEventListener("submit", function(event){
-//     event.preventDefault();
-//     var name = document.getElementById("nome").value
-    
-//     var dados = {
-//         name: name,
-//     };
-//     // transforma em JSON
-//     const data = JSON.stringify(dados)
 
-//     localStorage.setItem("dadosFormulario", data)
-    
-//     console.log(data)
-// })
---
+++ Front-Estatico/aula 04/teste.js
@@ -0,0 +1,10 @@
+console.clear()
+let nome = 'Iza'
+
+function mudarNome(){
+    var nome = 'Vanessa'
+    console.log('Função',nome)
+}
+
+mudarNome()
+console.log (nome)
